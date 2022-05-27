c59e081edef7f31e84764a72043f6909a8f4d19c
Max W <mwinter@wesleyan.edu>
2022-05-26 19:55:45 -0500

    added header page menus

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,5 +3,35 @@ import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
 
 const Header = () => {
-    
+    /*
+        menus to include in the header:
+        Towing Login, Building Login, Dashboard, Logout
+
+    */
+
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+    };
+
+    return (
+        <header>
+            <nav>
+                {Auth.loggedIn() ? (
+                    <>
+                        <Link to="/dashboard">My Dashboard</Link>
+                        <a href="/" onClick={logout}>Logout</a>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/towingLogin">Towing Login</Link>
+                        <Link to="/buildingLogin">Building Login</Link>
+
+                    </>
+                )}
+            </nav>
+        </header>
+    )
 };
+
+export default Header;
