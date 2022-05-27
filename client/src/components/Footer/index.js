88f91d18885b7c93d1cf1804ad213065ce27ffe5
Max W <mwinter@wesleyan.edu>
2022-05-26 19:59:47 -0500

    added footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+
+    return (
+        <footer>
+            <div>&copy;2022 Don't Get Towed LLC</div>
+        </footer>
+    )
+};
+
+export default Footer;
