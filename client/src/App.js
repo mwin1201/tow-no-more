c3ab58d1006000a082694353b4f5da907284cd2c
Max W <mwinter@wesleyan.edu>
2022-06-20 15:59:30 -0500

    created working app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { 
+  ApolloProvider, // React component to provide data to other components
+  ApolloClient,  // constructor function to help initialize the connection to the GraphQL API server
+  InMemoryCache,  // enables Apollo Client instance to cache API response data - perform requests more efficiently
+  createHttpLink  // allows us control over how the Apollo Client makes a request
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// pages
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+
+// components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const httpLink = createHttpLink({
+  uri: "/graphql"
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}`: ""
+    }
+  };
+});
+
+//link authLink and httpLink so every request retrieves the token
+//and sets the request headers before making the request to the API.
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />            
+        </Routes>
+        <Footer />
+      </Router>
+    </ApolloProvider>
   );
 }
 
